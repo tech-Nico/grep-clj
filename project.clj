4ec89a06019268fcb57c0a0fec50600e4e1fc849
Nico Balestra <nicobalestra@gmail.com>
2012-06-16 00:36:14 +0100

    Added main class

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject Grep "1.0.0-SNAPSHOT"
   :description "A simple grep in Clojure using javax.regexp"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main Grep.core)
