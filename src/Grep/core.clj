ed5d44642efce4a9fe45ec65b1088c4bf7b83820
Nico Balestra <nicobalestra@gmail.com>
2012-06-19 23:15:28 +0100

    Added a  bit of logic to handle the recursive scan of dir and a bit of refactoring and parameters handling.

--- src/Grep/core.clj
+++ src/Grep/core.clj
@@ -67,9 +67,13 @@ has been found in the line, returns the line with the match ***marked***"
  ;; grep simply scan the given fine and search for the specified reg-ex
 ;; It returns a list of couples [col row] where each matching has been found
 ;; this is a test this is a test this is a test this is a test
-(defn process-file [filename search-for]
+(defn grep [file search-for]
+  "Check if the given filename exists and call the function to
+search for the matches. It has side effects since it's printing
+an error message if the file doesn't exist. Maybe it should return a value
+or throw an exception."
   
-    (with-open [rdr (BufferedReader. (FileReader. filename))]
+    (with-open [rdr (BufferedReader. (FileReader. file))]
 
       ;;This should be refactored by using the -> (or ->> ?) macro
       (show-results
@@ -80,18 +84,6 @@ has been found in the line, returns the line with the match ***marked***"
                                   (search-line line search-for (+ 1 line-num)))) (line-seq rdr)))))
 
             ))
-
-(defn grep [filename search-for]
-  "Check if the given filename exists and call the function to
-search for the matches. It has side effects since it's printing
-an error message if the file doesn't exist. Maybe it should return a value
-or throw an exception."
-  
-  (if (. (File. filename) exists)
-    (process-file filename search-for)
-    (println "File [" filename "] not found")
-    )
-  )
 
 (defn as-file [s]
   "Code courtesy of bpsm (https://gist.github.com/bpsm)
@@ -109,19 +101,39 @@ Given a directory, returns a map with the list of files traversing all subdirect
     (concat files (mapcat walk subdirs))))
 
 
-(defn grep-recursive [pattern]
-  (println "TO BE DONE"))
+(defn grep-recursive [dir pattern]
+  (-> dir
+      walk
+      (apply #(grep %1 pattern))))
 
 (defn -main
-  ([] (println "Syntax: grep [filename|-r] reg-expn\n"
-               "-r\t\tSearch all files recursively\n"
-               "filename\t\tSpecify the filename to search into\n"
-               "reg-exp\t\tThe regular expression to search for"))
+  ([] (println "Syntax: grep [filename|[-r dir]] reg-expn\n"
+               "-d          Search all files in the specified [dir] and recursively in all\n"
+               "            subdirectories \n"
+               "filename    Specify the filename to search into\n"
+               "reg-exp     The regular expression to search for"))
+  ([param]
+     (-main))
   ([filename pattern]
-     (if  (= filename "-r")
-       (grep-recursive pattern)
-       (grep filename pattern)
+     (if  (= filename "-d")
+       (grep (as-file filename) pattern)
+       (do
+         (println "Invalid number of arguments")
+         (-main)
+         )
        ))
   ;;simply ignore any other arguments
-  ([filename pattern args &](grep filename pattern )
-  ))
+  ([option dir pattern]
+     (if (= option "-d")
+       (grep-recursive (as-file dir) pattern)
+       (do
+         (println "Invalid option")
+         (-main)
+         )
+     )
+     )
+  ([option dir pattern args &]
+     (println "Invalid number of arguments")
+     (-main)
+     )  
+  )
